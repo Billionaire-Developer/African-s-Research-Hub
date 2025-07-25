3c8ef9aed5cf404e36b7fc91d434007028ade92a
Donald <donalddbanda@gmail.com>
2025-07-25 08:14:35 +0200

    initialized mial

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS # type: ignore
 from flask_login import LoginManager # type: ignore
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail # type: ignore
 
 app = Flask(__name__)
 CORS(app)
@@ -12,5 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'  # Redirect to login page if not authenticated
+mail = Mail(app)
 
 from app import routes, models
