cf89caaa8d7a60e8351abdba883790bf245852c2
Donald <donalddbanda@gmail.com>
2025-07-25 08:43:37 +0200

    Created app/email_service.py Refactored imports and initialized Flask-Mail in backend/app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,15 +4,19 @@ from flask_cors import CORS # type: ignore
 from flask_login import LoginManager # type: ignore
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail # type: ignore
+from app.email_service import mail
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object('config.Config')
+
+# Initialize extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'  # Redirect to login page if not authenticated
-mail = Mail(app)
+
+# Initialize Flask-Mail
+mail.init_app(app)
 
 from app import routes, models
